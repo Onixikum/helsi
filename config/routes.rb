e61eee824628f412ee19a067bea2905531b3efbf
Onixikum <dragonix1988@gmail.com>
2022-12-27 12:18:51 +0200

    Add Profile to user models

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,14 @@
 Rails.application.routes.draw do
   root to: 'home#index'
 
-  devise_for :users
-  devise_for :doctors
+  devise_for :users, controllers: {
+    registrations: 'persons/registrations'
+  }
+
+  devise_for :doctors, controllers: {
+    registrations: 'persons/registrations'
+  }
+
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
 end
