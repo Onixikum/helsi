be1c3383004e696d26a44deb90bda2946bf66a85
Onixikum <dragonix1988@gmail.com>
2022-12-27 15:08:35 +0200

    Add doctors controller and fix views

---
+++ app/controllers/doctors/home_controller.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+module Doctors
+  class HomeController < BaseController
+    def show
+    end
+  end
+end
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,5 +3,6 @@
 class HomeController < ApplicationController
   def welcome
     redirect_to user_root_path if current_user
+    redirect_to doctor_root_path if current_doctor
   end
 end
