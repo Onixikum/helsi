a0563ce777b800d1006084cd44230cfee090a46a
Onixikum <dragonix1988@gmail.com>
2022-12-28 17:40:26 +0200

    Add user lists to ActiveAdmin

---
+++ app/admin/users.rb
@@ -0,0 +1,39 @@
+# frozen_string_literal: true
+
+ActiveAdmin.register User do
+  filter :phone
+  filter :created_at
+
+  index do
+    selectable_column
+    id_column
+    column(:full_name) { |user| user.profile }
+    column :phone
+    actions
+  end
+
+  show do
+    attributes_table do
+      row(:full_name) { |user| user.profile }
+      row :phone
+      row :created_at
+      row :update_at
+    end
+    active_admin_comments
+  end
+
+  form do |f|
+    f.inputs do
+      f.inputs do
+        f.has_many :profile, allow_destroy: true do |t|
+          t.input :first_name
+          t.input :last_name
+        end
+      end
+      f.input :phone
+      f.input :password
+      f.input :password_confirmation
+    end
+    f.actions
+  end
+end
