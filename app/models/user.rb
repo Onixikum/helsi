3c45786963bf635d6906329b655612d33ee12f33
Onixikum <dragonix1988@gmail.com>
2023-01-04 19:58:16 +0200

    Add appointments

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,8 @@ class User < ApplicationRecord
   include ExcludeEmail
 
   has_one :profile, as: :person, dependent: :destroy
+  has_many :appointments
+  has_many :physicians, through: :appointments
 
   accepts_nested_attributes_for :profile
 
